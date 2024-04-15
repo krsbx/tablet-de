ed303495bb21a9acc550a3a3112bf4fe4a28e514
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-04-15 23:18:43 +0700

    fix: code-updater wont run on setup

--- distro/code-server.sh
+++ distro/code-server.sh
@@ -10,6 +10,10 @@ echo "Copying code-server runner to \$HOME/bin"
 cp code $HOME/bin/code
 chmod +x $HOME/bin/code
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 echo "Downloading latest code-server release"
 node $HOME/bin/code-updater
 
